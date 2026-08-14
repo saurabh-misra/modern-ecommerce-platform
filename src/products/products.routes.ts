dc2feb60c4888ecd67170fe723ebfb960373153d
Saurabh Misra <saurabhmisra.dev@gmail.com>
2026-08-14 16:26:08 +0530

    feat(products): Modify API response shapes to be compliant with widely used conventions.

--- src/products/products.routes.ts
+++ src/products/products.routes.ts
@@ -17,23 +17,35 @@ const mockData: Product[] = [
 ];
 
 router.get( '/', ( _req, res ) => {
-    res.json( mockData );
+    res.json( { data: mockData } );
 });
 
 router.get( '/:id', ( req, res ) => {
     const id = Number( req.params.id );
 
     if( Number.isNaN( id ) ){
-        return res.status( 400 ).json( { status: "error", message: "Invalid request." } );
+        return res.status( 400 )
+            .json({ 
+                error: { 
+                    code: "PRODUCT_ID_INVALID", 
+                    message: "Product ID is invalid." 
+                }
+            });
     }
 
     const product = mockData.find( p => p.id === id );
 
     if( !product ) {
-        return res.status( 404 ).json({ status: "error", message: "Not found." });
+        return res.status( 404 )
+            .json({
+                error: { 
+                    code: "PRODUCT_NOT_FOUND", 
+                    message: "Product not found." 
+                }
+            });
     }
 
-    res.json( product );
+    res.json( { data: product } );
 });
 
 export default router;
